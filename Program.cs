d67a3dd6d1bb4466de5f51b146de713c8197ff81
Philipe-Berube <philipe.berube@hotmail.com>
2025-12-03 10:30:59 -0500

    player struct

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿
 
 //hello this is our final project
+
+struct Player 
+{
+    string Id;
+    string Name;
+    int NumOfHangmanGamesPlayed;
+    int NumOfTicTacToeGamesPlayed;
+    int HangmanScore;
+    int TicTacToeScore;
+    float AvgScore;
+    bool IsSuspended;
+}
