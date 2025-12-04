2d8ff1ccca78d71be5eb48b4a9498e1d0c2381ef
Vincent Bischoff <vincent.bischoff@lcieducation.net>
2025-12-04 15:24:54 -0500

    moi

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@
 
 //hello this is our final project
 
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Serialization;
+
 Player[] listOfPlayers = new Player[50];
 int numOfPlayers = 0;
 
@@ -13,79 +16,16 @@ do
 {
     Console.WriteLine("Welcome to B&B Games would you like to enter\n1- admin menu\n2- the player menu\n3- exit");
     choice = Convert.ToInt32(Console.ReadLine());
-
+    
     switch (choice) 
     {
         case 1:
-            // Admin menu
-            Console.WriteLine ("Welcome to the admin menu");
-            for (int i = 0; i < 3;)
-            {
-                Console.WriteLine("Please enter the admin password:");
-                string inputPsswd = Console.ReadLine();
-                if (inputPsswd == psswd)
-                {
-                
-                    Console.WriteLine("You have been logged in to the admin menu");
-                    // Admin menu functionality goes here
-                    Console.WriteLine("what would you like to do\n1- View a profile\n2- Display all profiles" +
-                        "\n3- Run stats\n4- Un/Suspend player\n5- Return to Main Menu");
-
-                    int menuChoice = Convert.ToInt32(Console.ReadLine());
-
-                    switch (menuChoice)
-                    {
-                        case 1:
-                            // View a profile
-                            Console.WriteLine("Enter player ID to view profile:");
-                            string tempID = Console.ReadLine();
-                            GetPlayerID (tempID);
-                            if (GetPlayerID(tempID).Equals("-1"))
-                            {
-                                Console.WriteLine("Player ID not found.");
-                            }
-                            else
-                            {
-                                Console.WriteLine("Player ID found.");
-                                // Display player profile details here
-                                int id = Convert.ToInt32(GetPlayerID(tempID));
-
-                                PrintPlayerProfile(id);
-
-
-                            }
-                        break;
-                        case 2:
-                            // Display all profiles
-                            for (int j = 0; j < listOfPlayers.Length; j++)
-                            {
-                                PrintPlayerProfile(j);
-                            }
-                            break;
-                        case 3:
-                            // Run stats
-                            break;
-                        case 4:
-                            // Un/Suspend player
-                            break;
-                        case 5:
-                            // Return to Main Menu
-                            break;
-                    }
-
-                }
-                else
-                {
-                    Console.WriteLine("Access has been denied!");
-                    i++;
-                }
-            }
+            AdminMenu();
             break;
         case 2:
-            // Player menu
-            Console.WriteLine("Welcome to the player menu");
+            PlayerMenu();
+            
 
-            DisplayT3Board();
             break;
         case 3:
             // Exit
@@ -202,8 +142,91 @@ void AdminMenu()
         }
     }
 }
+void PlayerMenu()
+{
+    Console.WriteLine("Welcome to the player menu");
+    Console.WriteLine("What would you like to do \n1: creat account\n2: check account");
+    int input = Convert.ToInt32(Console.ReadLine());
+    switch (input)
+    {
+        case 1:
+            CreatAccount();
+            numOfPlayers++;
+
+            break;
+        case 2:
+            EnterAccount();
+            break;
+    }
+        
+}
+void CreatAccount()
+{
+    bool doesntOverlap = false;
+    string tempId;
+    string tempName;
+    char answr;
+    do
+    {
+        Console.WriteLine("please enter a new ID");
+        tempId = Console.ReadLine();
+        for (int i = 0; i < listOfPlayers.Length; i++)
+        {
+            if (tempId == listOfPlayers[i].Id)
+            {
+                doesntOverlap = true;
+                break;
+            }
+        }
+    }while (doesntOverlap);
+    
+    do
+    {
+        Console.WriteLine("please enter your username");
+        tempName = Console.ReadLine();
+        do
+        {
+            Console.WriteLine("are you sure of that name (y/n)");
+            answr = Convert.ToChar(Console.ReadLine().ToLower());
+
+        } while (answr != 'y' && answr != 'n');
+    } while (answr == 'n');
+
+    listOfPlayers[numOfPlayers].Id = tempId;
+    listOfPlayers[numOfPlayers].Name = tempName;
+
+    
 
 
+}
+void EnterAccount()
+{
+    string tempId;
+    string id;
+    int Id;
+
+    for (int i = 0;i <=3 ;i++)
+    {
+        do
+        {
+            Console.WriteLine("please enter your Id");
+            tempId = Console.ReadLine();
+            id = GetPlayerID(tempId);
+        } while (id == "-1");
+        if (id != "-1")
+        {
+            Id = Convert.ToInt32(GetPlayerID(tempId));
+            PrintPlayerProfile(Id);
+            break;
+        }
+        else { Console.WriteLine("please enter a valid Id"); }
+    }
+    
+    Console.WriteLine(
+
+
+
+}
 
 
 void DisplayT3Board()
@@ -211,19 +234,37 @@ void DisplayT3Board()
     char temp = '1';
     // Tic Tac Toe game logic goes here
     char[,] ticTacToeBoard = new char[3, 3];
+    for (int rows = 0; rows < ticTacToeBoard.GetLength(0); rows++)
+    {
+        for (int collums = 0; collums < ticTacToeBoard.GetLength(1); collums++)
+        {
+            ticTacToeBoard[rows, collums] = temp++;
+
+
+        }
+    }
     char currentPlayer = 'X';
     Console.WriteLine("Starting Tic Tac Toe...");
     Console.WriteLine("setting up a new board...\n");
+
+    String[] player = new string[2];
+
+    Console.WriteLine("hello player 1 please enter your name:");
+    player[0] = Console.ReadLine();
+    Console.WriteLine("hello player 2 please enter your name:");
+    player[1] = Console.ReadLine();
+
+
     for (int currentplayer = 0; currentplayer < 3; currentplayer++)
     { 
         currentplayer = currentplayer % 2;
         if (currentplayer == 0) { currentPlayer = 'X'; }
         else currentPlayer = 'O';
-        for (int rows = 0; rows < ticTacToeBoard.GetLength(0); rows++)
+
+        for (int rows = 0; rows < ticTacToeBoard.GetLength(0); rows++)// prining board 
         {
             for (int collums = 0; collums < ticTacToeBoard.GetLength(1); collums++)
             {
-                ticTacToeBoard[rows, collums] = temp++;
                 Console.Write(ticTacToeBoard[rows, collums]);
 
             }
@@ -232,12 +273,42 @@ void DisplayT3Board()
         }
         Console.WriteLine();
 
+        switch (TicTacToeInput(ticTacToeBoard))
+        {
+            case '1':
+                SwapInBoard(ticTacToeBoard, 0, 0, currentPlayer);
+                break;
+            case '2':
+                SwapInBoard(ticTacToeBoard, 0, 1, currentPlayer);
+                break;
+            case '3':
+                SwapInBoard(ticTacToeBoard, 0, 2, currentPlayer);
+                break;
+            case '4':
+                SwapInBoard(ticTacToeBoard, 1, 0, currentPlayer);
+                break;
+            case '5':
+                SwapInBoard(ticTacToeBoard, 1, 1, currentPlayer);
+                break;
+            case '6':
+                SwapInBoard(ticTacToeBoard, 1, 2, currentPlayer);
+                break;
+            case '7':
+                SwapInBoard(ticTacToeBoard, 2, 0, currentPlayer);
+                break;
+            case '8':
+                SwapInBoard(ticTacToeBoard, 2, 1, currentPlayer);
+                break;
+            case '9':
+                SwapInBoard(ticTacToeBoard, 2, 2, currentPlayer);
+                break;
+        }
 
-        Console.WriteLine("hello player 1 please enter your name:");
-        string player1Name = Console.ReadLine();
-        Console.WriteLine("PLease enter the row followed by the collum you want to place your peace in (0-2)");
-        int r = Convert.ToInt32(Console.ReadLine());
-        int c = Convert.ToInt32(Console.ReadLine());
+
+
+        //Console.WriteLine("PLease enter the row followed by the collum you want to place your peace in (0-2)");
+        //int r = Convert.ToInt32(Console.ReadLine());
+        //int c = Convert.ToInt32(Console.ReadLine());
 
 
 
@@ -247,12 +318,55 @@ void DisplayT3Board()
     }
 }
 
+
+
+
+char[,] SwapInBoard(char[,] board,int r,int c, char swap)
+{
+    board[r,c] = swap;
+    return board;
+}
+
+
+
+char TicTacToeInput(char[,] board)
+{
+    char playerchoice;
+    bool spaceTaken = true;
+    do
+    {
+        do
+        {
+            Console.WriteLine("please enter which space you want to play in");
+            playerchoice = Convert.ToChar(Console.ReadLine());
+
+        } while (playerchoice != '1' && playerchoice != '2' && playerchoice != '3' && playerchoice != '4' && playerchoice != '5' && playerchoice != '6' && playerchoice != '7' && playerchoice != '8' && playerchoice != '9');
+
+        for (int i = 0; i < board.GetLength(0); i++)
+        {
+            for (int j = 0; j < board.GetLength(1); j++)
+            {
+                if (board[i, j] == playerchoice)
+                {
+                    spaceTaken = false; 
+                }
+            }
+        }
+
+    }while (spaceTaken);
+
+    return playerchoice;
+}
+
+
+
 bool IsWinner(char[,] board)
 {
     if (board[0, 0] == board[0, 1] && board[0, 1] == board[0, 2] || board[1, 0] == board[1, 1] && board[1, 1] == board[1, 2] 
-        || board[2, 0] == board[2, 1] && board[2, 1] == board[2, 2] || board[0, 0] == board[1, 0] && board[1, 0] == board[2, 0] 
-        || board[1, 0] == board[1, 1] && board[1, 1] == board[2, 1] || board[2, 0] == board[1, 2] && board[1, 2] == board[2, 2] 
-        || board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2] || board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
+        || board[2, 0] == board[2, 1] && board[2, 1] == board[2, 2] ||/*horizontal*/
+        board[0, 0] == board[1, 0] && board[1, 0] == board[2, 0] || board[1, 0] == board[1, 1] && board[1, 1] == board[2, 1] 
+        || board[2, 0] == board[1, 2] && board[1, 2] == board[2, 2] /*vartical*/
+        || board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2] || board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])//diagonal
     {
         return true;
     }
